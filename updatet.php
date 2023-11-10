510f766537fe955ca40ef29cbf0369363c4385b0
khoido1303 <144263267+khoido1303@users.noreply.github.com>
2023-11-10 16:49:18 +0700

    Delete updatet.php

--- updatet.php
+++
@@ -1,5 +0,0 @@
-$url = 'https://raw.githubusercontent.com/khoido1303/khoido1303/main/golikeupdate.php';
-       	$file_name = basename($url); 
-    if (file_put_contents("$file_name", file_get_contents($url))) 
-    { echo "Tải File thành công name : $file_name \n"; } else
-    { echo "Tải File thất bại\n."; }
